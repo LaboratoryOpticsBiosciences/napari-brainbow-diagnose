249a2739776c689c1a0270a57b6a618d7ba14e10
ClementCaporal <caporal.clement@gmail.com>
2022-11-23 17:37:20 +0100

    add rgb sample

--- src/napari_brainbow_diagnose/__init__.py
+++ src/napari_brainbow_diagnose/__init__.py
@@ -4,7 +4,7 @@ except ImportError:
     __version__ = "unknown"
 
 from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
+from ._sample_data import make_rgb_cube_data
 from ._widget import ExampleQWidget, example_magic_widget
 from ._writer import write_multiple, write_single_image
 
@@ -12,7 +12,7 @@ __all__ = (
     "napari_get_reader",
     "write_single_image",
     "write_multiple",
-    "make_sample_data",
+    "make_rgb_cube_data",
     "ExampleQWidget",
     "example_magic_widget",
 )
--- src/napari_brainbow_diagnose/_sample_data.py
+++ src/napari_brainbow_diagnose/_sample_data.py
@@ -11,11 +11,19 @@ from __future__ import annotations
 import numpy
 
 
-def make_sample_data():
-    """Generates an image"""
+def make_rgb_cube_data():
+    """Generates an rgb cube"""
     # Return list of tuples
     # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
     # Check the documentation for more information about the
     # add_image_kwargs
     # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+
+    rgb_size = 256
+    step = numpy.linspace(0, 255, rgb_size)
+    rgb_cube = numpy.meshgrid(step, step, step)
+    return [
+        (rgb_cube[0], {"colormap": "red", "blending": "additive"}),
+        (rgb_cube[1], {"colormap": "green", "blending": "additive"}),
+        (rgb_cube[2], {"colormap": "blue", "blending": "additive"}),
+    ]
