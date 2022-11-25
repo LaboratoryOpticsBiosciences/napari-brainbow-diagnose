8d0839a1642e10bbaf384f7c352adee959baea59
clement <caporal.clement@gmail.com>
2022-11-25 13:59:39 +0100

    remove unused test and skel

--- src/napari_brainbow_diagnose/_tests/test_sample_data.py
+++ src/napari_brainbow_diagnose/_tests/test_sample_data.py
@@ -1,7 +1,14 @@
-# from napari_brainbow_diagnose import make_sample_data
-
-# add your tests here...
+from napari_brainbow_diagnose import make_rgb_cube_data
 
 
-def test_something():
-    pass
+def test_make_rgb_cube_data(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
+
+    # this time, our widget will be a MagicFactory or FunctionGui instance
+    loader = make_rgb_cube_data()
+
+    # if we "call" this object, it'll execute our function
+    loader(viewer)
+
+    # read captured output and check that it's as we expected
+    assert len(viewer.layers) == 3
