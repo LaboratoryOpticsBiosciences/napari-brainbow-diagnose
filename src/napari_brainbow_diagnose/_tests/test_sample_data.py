9cde84d632da03638bd5fa0aaccb4bc6b9e6cbe4
clement <caporal.clement@gmail.com>
2022-11-25 14:07:00 +0100

    correct load data test

--- src/napari_brainbow_diagnose/_tests/test_sample_data.py
+++ src/napari_brainbow_diagnose/_tests/test_sample_data.py
@@ -2,13 +2,9 @@ from napari_brainbow_diagnose import make_rgb_cube_data
 
 
 def test_make_rgb_cube_data(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    loader = make_rgb_cube_data()
-
-    # if we "call" this object, it'll execute our function
-    loader(viewer)
+    # call the sample
+    layers = make_rgb_cube_data()
 
     # read captured output and check that it's as we expected
-    assert len(viewer.layers) == 3
+    assert len(layers) == 3
