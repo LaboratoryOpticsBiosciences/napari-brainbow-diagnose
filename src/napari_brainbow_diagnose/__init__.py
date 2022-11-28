749a8bde806c61a88cf27a305c277fb43c32a66e
Hugo Blanc <hugo.blanc@polytechnique.edu>
2022-11-28 16:30:44 +0100

    add chroms data sample to sample data

--- src/napari_brainbow_diagnose/__init__.py
+++ src/napari_brainbow_diagnose/__init__.py
@@ -6,12 +6,13 @@ except ImportError:
 from ._channel_ROI import channel_ROI_widget
 from ._channel_space import channel_space_widget
 from ._contrast import contrast_widget
-from ._sample_data import make_rgb_cube_data
+from ._sample_data import make_rgb_cube_data, load_chroms_data_sample
 from ._tooltip_pointer import tooltip_pointer_widget
 
 __all__ = (
     "tooltip_pointer_widget",
     "make_rgb_cube_data",
+    "load_chroms_data_sample",
     "contrast_widget",
     "channel_space_widget",
     "channel_ROI_widget",
