506dc50725a347ef58aed59e5413cb3ee01d5b57
ac744 <anatole.chessel@polytechnique.edu>
2022-12-07 17:13:08 +0100

    small bugs, still theta, phi compute issues

--- src/napari_brainbow_diagnose/_channel_space.py
+++ src/napari_brainbow_diagnose/_channel_space.py
@@ -72,25 +72,13 @@ class channel_space_widget(QWidget):
         self.redraw()
 
 
-    def redraw(self, rebuild_gui=True):
+    def redraw(self, rebuild_gui=True,rlim=None):
         # add a new plot to the graphics_widget or empty the old plot
         if not hasattr(self, "plot"):
             self.plot = self.graphics_widget.addPlot()
         else:
             self.plot.clear()
-            
-        # determine min/max intensity of all shown layers
-        all_minimum = None
-        all_maximum = None
-        for layer in self.selected_image_layers():
-            minimum, maximum = min_max(layer.data)
-            if all_minimum is None or all_minimum > minimum:
-                all_minimum = minimum
-            if all_maximum is None or all_maximum < maximum:
-                all_maximum = maximum
-        all_minimum = np.floor(all_minimum) if all_minimum is not None else 0
-        all_maximum = (all_maximum if all_maximum is not None else all_minimum + 1)
-        
+                   
         
         #find colors            
         colors = []
@@ -99,68 +87,74 @@ class channel_space_widget(QWidget):
             color = np.asarray(colormap[-1, 0:3]) * 255
             colors.append(color)
             
+            
+            
+            
+        xyz=[]
+        cur=self.viewer.dims.point
+        for layer in self.selected_image_layers():
+            xyz.append(np.clip(layer.data[int(cur[0]),:,:],layer.contrast_limits[0],layer.contrast_limits[1]))
+        xyz=np.array(xyz).reshape((3,-1)).transpose().astype('float')
+        rtp=toSpherical_np(xyz[:,:])  
+        
+        rmax=rtp[:,0].max()
+        
+        if rlim !=None:
+            rtp=rtp[rtp[:,0]>rlim[0],:]
+            rtp=rtp[rtp[:,0]<rlim[1],:]
+
+        if rlim ==None:
+            rlim=[rtp[:,0].min(),rtp[:,0].max()]        
+        self.plot.plot(rtp[:,1],rtp[:,2],pen=None,symbol='o',symbolSize=0.1,alpha=0.01)
+
         # update sliders
         if rebuild_gui:
             layout = self.sliders.layout()
             for i in reversed(range(layout.count())):
                 layout.itemAt(i).widget().setParent(None)
-            for i, layer in enumerate(self.selected_image_layers()):
-
-                row = LayerContrastLimitsWidget(
-                    layer, colors[i], all_minimum, all_maximum, self
+            row = RadiusRGBLimitsWidget(rmax,rlim, self
                 )
 
-                layout.addWidget(row)
-            
-            
-            
-        xyz=[]
-        for layer in self.selected_image_layers():
-            xyz.append(np.clip(layer.data[:],layer.contrast_limits[0],layer.contrast_limits[1]))
-        xyz=np.array(xyz).reshape((3,-1)).transpose().astype('float')
-        rtp=toSpherical_np(xyz[::100,:])        
-        
-        self.plot.plot(rtp[:,1],rtp[:,2],pen=None,symbol='o',symbolSize=0.1,alpha=0.01)
+            layout.addWidget(row)
         
         
 #from https://github.com/haesleinhuepf/napari-brightness-contrast  
-class LayerContrastLimitsWidget(QWidget):
+class RadiusRGBLimitsWidget(QWidget):
     """
     This widget corresponds to a single line represeting a layer with the option to configure min/max contrast limits.
     """
 
-    def __init__(self, layer, color, all_minimum, all_maximum, gui):
+    def __init__(self,rmax,rlim, gui):
         super().__init__(gui)
 
         self.setLayout(QHBoxLayout())
 
-        lbl = QLabel(layer.name)
-        lbl.setStyleSheet("color: #%02x%02x%02x" % tuple(color.astype(int)))
+        lbl = QLabel('Radius')
+        #lbl.setStyleSheet("color: #%02x%02x%02x" % tuple(color.astype(int)))
         self.layout().addWidget(lbl)
 
         # show min/max intensity
         lbl_min = QLabel()
-        lbl_min.setText("{:.2f}".format(layer.contrast_limits[0]))
+        lbl_min.setText("{:.2f}".format(0))
         lbl_max = QLabel()
-        lbl_max.setText("{:.2f}".format(layer.contrast_limits[1]))
+        lbl_max.setText("{:.2f}".format(rmax))
 
         # allow to tune min and max within one slider
         slider = QDoubleRangeSlider()
         slider.setOrientation(Qt.Horizontal)
-        slider.setMinimum(all_minimum)
-        slider.setMaximum(all_maximum)
-        slider.setValue(layer.contrast_limits)
-        slider.setSingleStep((all_maximum - all_minimum) / 1000)
+        slider.setMinimum(0)
+        slider.setMaximum(rmax)
+        slider.setValue(rlim)
+        slider.setSingleStep(rmax / 1000)
 
         # update on change
         def value_changed(): 
-            layer.contrast_limits = slider.value()
             lbl_min.setText("{:.2f}".format(slider.value()[0]))
             lbl_max.setText("{:.2f}".format(slider.value()[1]))
-            gui.redraw(rebuild_gui=False)
+            gui.redraw(rlim=slider.value(),rebuild_gui=False)
 
         slider.valueChanged.connect(value_changed)
-
+        
         self.layout().addWidget(lbl_min)
         self.layout().addWidget(slider)
         self.layout().addWidget(lbl_max)
