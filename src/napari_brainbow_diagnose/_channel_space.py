e15cbb80df0024ba6ecc28030e117c037d5b4bec
clement <caporal.clement@gmail.com>
2023-01-25 02:30:48 +0100

    fix plugin intern path

---
+++ src/napari_brainbow_diagnose/_channel_space.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+
+from magicgui import magic_factory
+
+if TYPE_CHECKING:
+    import napari
+
+
+@magic_factory
+def channel_space_widget(img_layer: "napari.layers.Image"):
+    """
+    User interface to select a region of interest in the channel space.
+    """
+    print(f"you have selected {img_layer}")
