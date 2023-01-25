7308f5cf470081e2f6dd1aeb8cdb623e80f2f71d
clement <caporal.clement@gmail.com>
2023-01-25 01:25:35 +0100

    add documentation

--- src/napari_brainbow_diagnose/__init__.py
+++ src/napari_brainbow_diagnose/__init__.py
@@ -7,12 +7,17 @@ from ._channel_density import DensityWidget
 from ._channel_ROI import channel_ROI_widget
 from ._channel_space import channel_space_widget
 from ._contrast import contrast_widget
-from ._sample_data import load_chroms_data_sample, make_rgb_cube_data
+from ._sample_data import (
+    fetch_chroms_data,
+    load_chroms_data_sample,
+    make_rgb_cube_data,
+)
 from ._tooltip_pointer import tooltip_pointer_widget
 
 __all__ = (
     "tooltip_pointer_widget",
     "make_rgb_cube_data",
+    "fetch_chroms_data",
     "load_chroms_data_sample",
     "contrast_widget",
     "channel_space_widget",
