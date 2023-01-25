b4ba59918f81c02c2b03ea8cbfef2f8f5128fb28
clement <caporal.clement@gmail.com>
2023-01-25 02:53:40 +0100

    remove version tag 'v'

--- src/napari_brainbow_diagnose/_sample_data.py
+++ src/napari_brainbow_diagnose/_sample_data.py
@@ -20,7 +20,7 @@ BRIAN = pooch.create(
     # The remote data is on Github
     base_url="https://github.com/LaboratoryOpticsBiosciences/"
     "napari-brainbow-diagnose/raw/{version}/data/",
-    version=f"v{__version__}",
+    version=__version__,
     # If this is a development version,
     # get the data from the "add_examples" branch
     version_dev="main",
